fcfa4a112f2b441319586569297090024d1643b1
Vlad Nichifor <vlad.c.nichifor@gmail.com>
2023-02-18 08:02:25 +0000

    change name for files and update day 1

---
+++ Python/Beginner Projects/Day 1/main_replit.py
@@ -0,0 +1,14 @@
+import datetime as dt
+
+
+print(f"Hello {input('What is your name? ')}")
+
+print(dt.date.today())
+
+print("""I am signing up for Replit's 100 days of Python challenge!/n
+I will make sure to spend some time every day coding along, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing online IDE so I can do this from my phone wherever I happend to be. 
+No excuses for not coding from the middle of a field!""")
+
+print("I am feeling motivated to finish!")
+print("You can follow my progress at replit.com/@Vladdstian")
