e648d377316773512d04eb4fea75cc2d18baa0d1
A M A U <97670164+AMAUXRIV@users.noreply.github.com>
2023-02-26 14:48:53 +0700

    Fix Clean Code (#5)
    * Fix Clean Code
    
    Signed-off-by: A M A U <97670164+AMAUXRIV@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: A M A U <97670164+AMAUXRIV@users.noreply.github.com>
    Co-authored-by: Vlad Nichifor <vlad.c.nichifor@gmail.com>

--- Python/Beginner Projects/Day 5/project.py
+++ Python/Beginner Projects/Day 5/project.py
@@ -1,33 +1,31 @@
 import random
+import string
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', '^Z^Zg', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
-
-print("Welcome to the password generator!\n")
-
-def check(type_of_char: str, list_of_char: list):
-    list_of = []
+def get_user_input(input_type: str) -> int:
     while True:
-        no_of = input(f"How many {type_of_char} would you like in your password? ")      
+        user_input = input(f"How many {input_type} would you like in your password? ")
         try:
-            no_of = int(no_of)
-        except:
+            user_input = int(user_input)
+            return user_input
+        except ValueError:
             print("Please enter a valid number.")
-            continue
-        break
-    for nom in range(no_of):
-        list_of.append(random.choice(list_of_char))
-    return list_of
 
-no_of_letters = check("letters", letters)
-no_of_symbols = check("symbols", symbols)
-no_of_numbers = check("numbers", numbers)
+def generate_password(length: int, letters: str, symbols: str, numbers: str) -> str:
+    password_chars = []
+    password_chars.extend(random.sample(letters, get_user_input("letters")))
+    password_chars.extend(random.sample(symbols, get_user_input("symbols")))
+    password_chars.extend(random.sample(numbers, get_user_input("numbers")))
+    password_chars.extend(random.sample(string.ascii_letters + string.digits, length - len(password_chars)))
+    random.shuffle(password_chars)
+    return "".join(password_chars)
 
-password_chars = no_of_letters + no_of_symbols + no_of_numbers
-random.shuffle(password_chars)
-password = ''.join(password_chars)
+def main():
+    letters = string.ascii_letters
+    symbols = "!#$%&()*+"
+    numbers = string.digits
+    length = get_user_input("length")
+    password = generate_password(length, letters, symbols, numbers)
+    print(f"\nYour password is: {password}.")
 
-print(f"\n\nYour password is: {password}.")
-
+if __name__ == "__main__":
+    main()
