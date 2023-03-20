11fd30d03fe189930f13d16869e123cb12a2eb58
Vladdstian <vlad.c.nichifor@gmail.com>
2023-03-20 15:13:24 +0000

    add week 2 topics

---
+++ R - Data Analytics/week_2_tidyverse.R
@@ -0,0 +1,12 @@
+# tidyverse tour
+install.packages('palmerpenguins')
+library('palmerpenguins')
+
+install.packages('ggplot2')
+library('ggplot2')
+
+ggplot(data=penguins, aes(x=flipper_length_mm, y=body_mass_g)) + geom_point()
+
+ggplot()
+
+ggplot(data=penguins, aes(x=flipper_length_mm, y=body_mass_g)) + geom_point()
